360c7c3b41154172a56ae8e0d7932a199a484bcf
Michal99Zajac <michal.99.zajac@gmail.com>
2023-06-26 23:35:36 +0200

    feat(app): use mock code

--- src/server.py
+++ src/server.py
@@ -1,74 +1,148 @@
+import argparse
 import asyncio
-from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack, RTCIceCandidate
-import websockets
 import json
+import logging
+import os
+import platform
+import ssl
 
-# Define a BYE constant
-BYE = "BYE"
+from aiohttp import web
+from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaPlayer, MediaRelay
+from aiortc.rtcrtpsender import RTCRtpSender
 
-
-class Signaling:
-    def __init__(self, websocket):
-        self.websocket = websocket
-
-    async def send(self, data):
-        await self.websocket.send(json.dumps(data))
-
-    async def receive(self):
-        msg = await self.websocket.recv()
-        return json.loads(msg)
+ROOT = os.path.dirname(__file__)
 
 
-class VideoTransformTrack(VideoStreamTrack):
-    def __init__(self, track):
-        super().__init__()  # don't forget this!
-        self.track = track
-
-    async def recv(self):
-        frame = await self.track.recv()
-
-        # simply pass the same frame forward
-        return frame
+relay = None
+webcam = None
 
 
-async def consume_signaling(pc, signaling):
-    while True:
-        obj = await signaling.receive()
+def create_local_tracks(play_from, decode):
+    global relay, webcam
 
-        if isinstance(obj, dict):
-            if "sdp" in obj and "type" in obj:
-                await pc.setRemoteDescription(RTCSessionDescription(**obj))
-
-                if obj["type"] == "offer":
-                    # send answer
-                    answer = await pc.createAnswer()
-                    await pc.setLocalDescription(answer)
-                    await signaling.send(
-                        {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
-                    )
-            elif "candidate" in obj:
-                candidate = RTCIceCandidate(**obj["candidate"])
-                await pc.addIceCandidate(candidate)
-            elif obj == BYE:
-                print("Exiting")
-                break
+    if play_from:
+        player = MediaPlayer(play_from, decode=decode)
+        return player.audio, player.video
+    else:
+        options = {"framerate": "30", "video_size": "640x480"}
+        if relay is None:
+            if platform.system() == "Darwin":
+                webcam = MediaPlayer("default:none", format="avfoundation", options=options)
+            elif platform.system() == "Windows":
+                webcam = MediaPlayer("video=Integrated Camera", format="dshow", options=options)
+            else:
+                webcam = MediaPlayer("/dev/video0", format="v4l2", options=options)
+            relay = MediaRelay()
+        return None, relay.subscribe(webcam.video)
 
 
-async def handle_signaling(websocket, path):
-    signaling = Signaling(websocket)
+def force_codec(pc, sender, forced_codec):
+    kind = forced_codec.split("/")[0]
+    codecs = RTCRtpSender.getCapabilities(kind).codecs
+    transceiver = next(t for t in pc.getTransceivers() if t.sender == sender)
+    transceiver.setCodecPreferences([codec for codec in codecs if codec.mimeType == forced_codec])
+
+
+async def index(request):
+    content = open(os.path.join(ROOT, "index.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+
+async def javascript(request):
+    content = open(os.path.join(ROOT, "client.js"), "r").read()
+    return web.Response(content_type="application/javascript", text=content)
+
+
+async def offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
     pc = RTCPeerConnection()
+    pcs.add(pc)
 
-    @pc.on("track")
-    def on_track(track):
-        print("Track %s received" % track.kind)
-        if track.kind == "video":
-            local_video = VideoTransformTrack(track)
-            pc.addTrack(local_video)
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        print("Connection state is %s" % pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
 
-    await consume_signaling(pc, signaling)
+    # open media source
+    audio, video = create_local_tracks(args.play_from, decode=not args.play_without_decoding)
+
+    if audio:
+        audio_sender = pc.addTrack(audio)
+        if args.audio_codec:
+            force_codec(pc, audio_sender, args.audio_codec)
+        elif args.play_without_decoding:
+            raise Exception("You must specify the audio codec using --audio-codec")
+
+    if video:
+        video_sender = pc.addTrack(video)
+        if args.video_codec:
+            force_codec(pc, video_sender, args.video_codec)
+        elif args.play_without_decoding:
+            raise Exception("You must specify the video codec using --video-codec")
+
+    await pc.setRemoteDescription(offer)
+
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps({"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}),
+    )
 
 
-start_server = websockets.serve(handle_signaling, "127.0.0.1", 8000)
+pcs = set()
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="WebRTC webcam demo")
+    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    parser.add_argument("--play-from", help="Read the media from a file and sent it."),
+    parser.add_argument(
+        "--play-without-decoding",
+        help=(
+            "Read the media without decoding it (experimental). "
+            "For now it only works with an MPEGTS container with only H.264 video."
+        ),
+        action="store_true",
+    )
+    parser.add_argument("--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)")
+    parser.add_argument(
+        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+    )
+    parser.add_argument("--verbose", "-v", action="count")
+    parser.add_argument("--audio-codec", help="Force a specific audio codec (e.g. audio/opus)")
+    parser.add_argument("--video-codec", help="Force a specific video codec (e.g. video/H264)")
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None
+
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_get("/", index)
+    app.router.add_get("/client.js", javascript)
+    app.router.add_post("/offer", offer)
+    web.run_app(app, host=args.host, port=args.port, ssl_context=ssl_context)
